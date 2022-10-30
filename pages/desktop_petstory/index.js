a8aca91d5a3aac506d563d512e82a1d41f4ccbbf
dmalenik <dmalenik@gmail.com>
2022-10-31 00:59:42 +0100

    refactor: improve footer structure

--- pages/desktop_petstory/index.js
+++ pages/desktop_petstory/index.js
@@ -1,3 +1,24 @@
 document
   .querySelector(".friend-feed-button")
   .addEventListener("click", () => window.open("../desktop_donate/index.html"));
+
+document
+  .querySelector(".donate-button")
+  .addEventListener("click", () => window.open("../desktop_donate/index.html"));
+
+const isValidEmail = (email) => {
+  const re = /\S+@\S+\.\S+/;
+
+  return re.test(email);
+};
+
+let em = document.querySelector("#email");
+let submitEmailBtn = document.querySelector(".submit");
+
+em.addEventListener("input", (event) => {
+  em.value = event.target.value;
+
+  isValidEmail(em.value)
+    ? submitEmailBtn.setAttribute("disabled", true)
+    : submitEmailBtn.removeAttribute("disabled");
+});
